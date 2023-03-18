facd59c9e930d9f75f635c045d8e2fdaf6f55b46
SomeStupidSongBird <563ftw@gmail.com>
2023-03-18 14:46:17 -0500

    changed to utilize updated menu

--- app/src/main/java/serverhost/ServerHost.java
+++ app/src/main/java/serverhost/ServerHost.java
@@ -1,14 +1,15 @@
 package serverhost;
 
 import serverhost.command_line.*;
+import serverhost.gui.*;
 
 public class ServerHost{
-
     public static void main(String[] args) {
         //check for commandline option otherwise startup the gui
+        ServerContainer serverContainer = new ServerContainer();
         if(args[0].equals("commandLine")){
-            Menu m = new Menu();
-            m.loadMenu();
+            Menu m = new Menu(serverContainer);
+            m.intro();
         }
     }
 }
