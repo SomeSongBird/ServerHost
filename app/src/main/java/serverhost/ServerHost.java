8e923edf109742293a29e5bd4c7804aa3ae075a3
SomeStupidSongBird <563ftw@gmail.com>
2023-03-18 18:02:14 -0500

    allows app to run with no extra args

--- app/src/main/java/serverhost/ServerHost.java
+++ app/src/main/java/serverhost/ServerHost.java
@@ -1,15 +1,17 @@
 package serverhost;
 
 import serverhost.command_line.*;
-import serverhost.gui.*;
+//import serverhost.gui.*;
 
 public class ServerHost{
     public static void main(String[] args) {
         //check for commandline option otherwise startup the gui
         ServerContainer serverContainer = new ServerContainer();
-        if(args[0].equals("commandLine")){
-            Menu m = new Menu(serverContainer);
-            m.intro();
+        if(args.length>0){
+            if(args[0].equals("commandLine")){
+                Menu m = new Menu(serverContainer);
+                m.intro();
+            }
         }
     }
 }
